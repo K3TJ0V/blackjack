4271a66bd8130dcedee5c05349bb4376f582a471
Wojciech Górka <wojciech.gorka@zszhs.krakow.pl>
2023-06-27 20:13:40 +0200

    table bot and player sides, card template, first listener with small animation

--- js/index.js
+++ js/index.js
@@ -1,3 +1,15 @@
 import "normalize.css";
 import "../scss/main.scss"
 
+const startButton = document.querySelector('.startButton-js');
+const playerSide = document.querySelector('.playerSide--js');
+const botSide = document.querySelector('.botSide--js');
+const cardTemplate = document.querySelector('#cardTemplate');
+
+
+startButton.addEventListener('click', ()=>{
+    startButton.style.transform = 'scale(0)';
+    setTimeout(() => {
+        startButton.style.display = 'none';
+    }, 500);
+}) 
