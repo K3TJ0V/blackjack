a3cf7dab61ff3ff7dc1cdae53450d95ccbbbd9fe
Wojciech Górka <wojciech.gorka@zszhs.krakow.pl>
2023-06-29 22:14:46 +0200

    bet menu, choosing bet value(without error handlers)

--- js/index.js
+++ js/index.js
@@ -8,6 +8,12 @@ const cardTemplate = document.querySelector('#cardTemplate');
 const hitButton = document.querySelector('.hit-js');
 const passButton = document.querySelector('.pass-js');
 const cash = document.querySelector('.cash-js');
+const betAmount = document.querySelector('.main__bet--amount');
+const clearingBet = document.querySelector('.betButton__0');
+const maxBet = document.querySelector('.betButton__MAX');
+
+let betButtons = [];
+let betValue = 0;
 
 
 startButton.addEventListener('click', ()=>{
@@ -15,7 +21,7 @@ startButton.addEventListener('click', ()=>{
     playerSide.classList.add('cardHoldersActivated');
     botSide.classList.add('cardHoldersActivated');
 
-    //showing buttons and balance
+    // showing buttons and balance
     setTimeout(() => {
         startButton.style.display = 'none';
         hitButton.style.transform = 'scale(1)';
@@ -27,3 +33,34 @@ startButton.addEventListener('click', ()=>{
         passButton.style.transform = 'scale(1)';
     }, 1500);
 }) 
+// donwloading all bet buttons
+for (let i = 1; i < 1001; i = i * 10) {
+    let x = document.querySelector('.betButton__add' +i);
+    betButtons.push(x);
+    console.log(betButtons);
+    let y = document.querySelector('.betButton__less' +i);
+    betButtons.push(y);
+}
+
+betButtons.forEach((button) =>{
+    button.addEventListener('click', (e)=>{
+        let innerString = e.target.innerHTML;
+        let value = innerString.substring(1, 5);
+        if (innerString.substring(1, -1) == "-"){
+            let x = parseInt(value);
+            let newValue = betValue - x;
+            betAmount.innerText = newValue;
+            betValue = newValue;
+        }else{
+            let x = parseInt(value);
+            let newValue = betValue + x;
+            betAmount.innerHTML = newValue;
+            betValue = newValue; 
+        }
+    })
+})
+
+clearingBet.addEventListener('click', ()=>{
+    betValue = 0;
+    betAmount.innerHTML = betValue;
+})
