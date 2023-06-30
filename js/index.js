c03622c6001062d97488916527a16e552a685302
Wojciech Górka <wojciech.gorka@zszhs.krakow.pl>
2023-07-01 00:03:24 +0200

    ok button function, deck of cards, card class

--- js/index.js
+++ js/index.js
@@ -8,13 +8,49 @@ const cardTemplate = document.querySelector('#cardTemplate');
 const hitButton = document.querySelector('.hit-js');
 const passButton = document.querySelector('.pass-js');
 const cash = document.querySelector('.cash-js');
-const betAmount = document.querySelector('.main__bet--amount');
+const betChosenAmount = document.querySelector('.main__bet--amount');
 const clearingBet = document.querySelector('.betButton__0');
 const maxBetButton = document.querySelector('.betButton__MAX');
+const betButtonsContainer = document.querySelector('.main__bet');
+const betApplyButton = document.querySelector('.main__bet--apply');
 
 let betButtons = [];
 let betValue = 0;
 let maxBetValue = parseInt(cash.innerHTML.substring(1, cash.innerHTML.length));
+// creating deck of cards
+let deck = [];
+let higherCardRanks = ['A','J','Q',"K"];
+class Card{
+    constructor(rank,value,color){
+        this.rank = rank;
+        this.value = value;
+        this.color = color;
+    }
+}
+for (let i = 2; i < 16; i++) {
+    if(i < 11){
+        // creating lower ranked cards 1-10
+        deck.push(new Card(i, i, 'heart'));
+        deck.push(new Card(i, i, 'clubs'));
+        deck.push(new Card(i, i, 'diamond'));
+        deck.push(new Card(i, i, 'spades'));
+    }else if(i > 11){
+        if (higherCardRanks[i-12] != 'A') {
+            // creating higher ranked cards without ace in this block
+            deck.push(new Card(higherCardRanks[i-12], 10, 'heart'));
+            deck.push(new Card(higherCardRanks[i-12], 10, 'clubs'));
+            deck.push(new Card(higherCardRanks[i-12], 10, 'diamond'));
+            deck.push(new Card(higherCardRanks[i-12], 10, 'spades'));
+        }else{
+            // adding aces with different value
+            deck.push(new Card(higherCardRanks[i-12], 11, 'heart'));
+            deck.push(new Card(higherCardRanks[i-12], 11, 'clubs'));
+            deck.push(new Card(higherCardRanks[i-12], 11, 'diamond'));
+            deck.push(new Card(higherCardRanks[i-12], 11, 'spades'));
+        }
+    }
+}
+console.log(deck);
 
 startButton.addEventListener('click', ()=>{
     startButton.style.transform = 'scale(0)';
@@ -32,6 +68,9 @@ startButton.addEventListener('click', ()=>{
     setTimeout(() => {
         passButton.style.transform = 'scale(1)';
     }, 1500);
+    setTimeout(() => {
+        betButtonsContainer.style.transform = 'scale(1)';
+    }, 2000);
 }) 
 // donwloading all bet buttons
 for (let i = 1; i < 1001; i = i * 10) {
@@ -54,10 +93,10 @@ betButtons.forEach((button) =>{
             if(newValue  < 0){
                 newValue = 0;
                 betValue = newValue;
-                betAmount.innerText = newValue;
+                betChosenAmount.innerText = newValue;
                 return newValue;
             }
-            betAmount.innerText = newValue;
+            betChosenAmount.innerText = newValue;
             betValue = newValue;
         }else{
             // x is a parsed text from button for ex. 1, 10
@@ -67,10 +106,10 @@ betButtons.forEach((button) =>{
             if(newValue > maxBetValue){
                 newValue = maxBetValue;
                 betValue = newValue;
-                betAmount.innerText = newValue;
+                betChosenAmount.innerText = newValue;
                 return newValue;
             }
-            betAmount.innerHTML = newValue;
+            betChosenAmount.innerHTML = newValue;
             betValue = newValue; 
         }
     })
@@ -78,9 +117,12 @@ betButtons.forEach((button) =>{
 // the rest buttons logic clearing bet and setting max value available 
 maxBetButton.addEventListener('click', ()=>{
     betValue = maxBetValue;
-    betAmount.innerHTML = betValue;
+    betChosenAmount.innerHTML = betValue;
 })
 clearingBet.addEventListener('click', ()=>{
     betValue = 0;
-    betAmount.innerHTML = betValue;
+    betChosenAmount.innerHTML = betValue;
+})
+betApplyButton.addEventListener('click', ()=>{
+    betButtonsContainer.style.transform = 'scale(0)';
 })
